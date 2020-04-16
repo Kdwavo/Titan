d6d2caa3ad30f244eada5532314f5e3a6febd4b0
Kdwavo <63588863+Kdwavo@users.noreply.github.com>
2020-04-16 20:32:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,58 +8,15 @@ client.on('ready', () => {
 	client.user.setGame('w!b (Message) - w&w 🎆',"https://www.twitch.tv/peery13");
 });
 
-
-client.on('message', message => { // هاذا للبرودكسات
-        var prefix = 'w!'; // هنا تقدر تغير البرفكس
-	var command = message.content.split(" ")[0];
-	if(command == prefix + 'b') { // الكوماند w!b
-		if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You don`t have **ADMINISTRATOR** permission!");
-		var args = message.content.split(' ').slice(1).join(' ');
-		if(message.author.bot) return;
-		if(!args) return message.channel.send(`**➥ Useage:** ${prefix}b كلامك`);
-		if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You don`t have **ADMINISTRATOR** permission!");
-		
-		let bcSure = new Discord.RichEmbed()
-		.setTitle(`:mailbox_with_mail: **هل انت متأكد انك تريد ارسال رسالتك الى** ${message.guild.memberCount} **عضو**`)
-		.setThumbnail(client.user.avatarURL)
-		.setColor('#4510a8')
-		.setDescription(`**\n:envelope: ➥ رسالتك**\n\n${args}`)
-		.setTimestamp()
-		.setFooter(message.author.tag, message.author.avatarURL)
-		
-		message.channel.send(bcSure).then(msg => {
-			msg.react('✅').then(() => msg.react('❎'));
-			message.delete();
-			
-			
-			let yesEmoji = (reaction, user) => reaction.emoji.name === '✅'  && user.id === message.author.id;
-			let noEmoji = (reaction, user) => reaction.emoji.name === '❎' && user.id === message.author.id;
-			
-			let sendBC = msg.createReactionCollector(yesEmoji);
-			let dontSendBC = msg.createReactionCollector(noEmoji);
-			
-			sendBC.on('collect', r => {
-				        message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-            var bc = new Discord.RichEmbed()
-            .setColor('#4510a8')
-       .setTitle('Broadcast')
-       .addField('Server', message.guild.name)
-       .addField('Sender', message.author.username)
-       .addField('Message',args)
-       .setThumbnail(message.author.avatarURL)
-       .setFooter('🖤 w&w is Life 🖤 ', message.guild.iconURL);
-            m.send(`${m}`,{embed: bc});
-        });
-				message.channel.send(`:timer: **يتم الان الارسال الى** \`\`${message.guild.memberCount}\`\` **عضو**`).then(msg => msg.delete(5000));
-				msg.delete();
-			})
-			dontSendBC.on('collect', r => {
-				msg.delete();
-				message.reply(':white_check_mark: **تم الغاء ارسال رسالتك بنجاح**').then(msg => msg.delete(5000));
-			});
-		})
-	}
+client.on('ready',async () => {
+let g = client.guilds.get("694888918331686943");
+let c = g.channels.get("694888919933911121");
+if(c.type === 'voice') {
+c.join();
+setInterval(() => {
+if(g.me.voiceChannel && g.me.voiceChannelID !== c.id || !g.me.voiceChannel) c.join();
+}, 1000);
+}
 });
 
 
